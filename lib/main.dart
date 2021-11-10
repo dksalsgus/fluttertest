d5217ed895607f0450e6d0603714fccf0ee2aedf
dksalsgus <alsgus2ek@naver.com>
2021-11-10 21:29:04 +0900

    set ElevatedButton onPressed function

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,42 @@ void main() => runApp(new MyApp());
  * StatelessWidget 상속
  */
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer 1 chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    // throw UnimplementedError();
-    return new MaterialApp(
+    var questions = [
+      '가장 좋아하는 색깔',
+      '가장 좋아하는 동물',
+    ];
+    return MaterialApp(
       // Scaffold 기본 UI 제공
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('My First App'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          // Column은 열로 배치 Row 행으로 배치
+          children: <Widget>[
+            Text('The Question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed:
+                  answerQuestion, // answerQuestion()는 바로 실행되기 떄문에 answerQuestion의 포인터만 onPressed에 준다
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'), // 익명함수
+            ),
+            ElevatedButton(
+                child: Text('Answer 3'),
+                onPressed: () {
+                  print('Answer 3 chosen!'); // 익명함수
+                }),
+          ],
+        ),
       ),
     );
   }
